3c1898dd3a4cbd77709568ea7ba2abc613a52ebd
lwx <1045670449@qq.com>
2022-05-30 20:31:35 +0800

    完成了index

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,15 @@
 $.ajaxPrefilter((options)=>{
 options.url='http://www.liulongbin.top:3007'+options.url
+if(options.url.includes('/my/')){
+    options.headers={
+        Authorization:localStorage.getItem('token')
+    }
+}
+options.complete=(res)=>{
+    if(res.responseJSON.status===1&&res.responseJSON.message==="身份认证失败！"){
+        localStorage.removeItem('token')
+        location.href='/login.html'
+    }
+}
 })
 //每次请求的时候都需要去添加根路径，比较的麻烦，如果根路径进行了修改，那么每个请求的页面都需要调整，那么jQuery中提供了一个 过滤器，可以帮我们统一去进行设置，而这个过滤器调用的时机是在我们调用 $.ajax() 之后，请求真正发给后台之前调用的： $.ajax() > ajaxPrefilter过滤器 -> 发送请求给服务器
---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+function getUserInfo(){
+    $.ajax({
+        type:"GET",
+        url:"/my/userinfo",
+        success:res=>{{
+            console.log(res);
+            if(res.status!==0){
+                return layer.msg(res.message)
+            }
+            layer.msg('信息获取成功')
+            renderAvtar(res.data)
+        }}
+    })
+}
+
+
+const renderAvtar=(user)=>{
+    // console.log(user);
+    let uname=user.nickname||user.username
+    // console.log(uname);
+    $('#welcome').html(`欢迎 ${uname}`)
+    if(user.user_pic!==null){
+        $('#layui-nav-img').attr('scr',user.user_pic)
+        $('.text-avatar').hide()
+    }else{
+        $('.text-avatar').html(uname[0].toUpperCase())
+        $('#layui-nav-img').hide()
+    }
+}
+$('#btnlogout').click(()=>{
+    layui.layer.confirm(
+        "确定退出登录？",
+        { icon: 3, title: "提示" },
+    function(index){
+        localStorage.removeItem('token')
+        location.href = "/login.html";
+    })
+})
+
+getUserInfo()
